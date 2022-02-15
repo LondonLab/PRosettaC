ea0d2da3e7e7040821898fe3302103d5539de37c
이용홍 <hong@pharmcadd.com>
2022-02-15 09:36:17 +0900

    feedback https://github.com/LondonLab/PRsettaC/pull/7

--- cluster/Cluster.py
+++ cluster/Cluster.py
@@ -54,7 +54,7 @@ class Cluster:
                         print("Not done yet")
                         if i == timeout:
                                 break
-                        time.sleep(1)
+                        time.sleep(1000)
                         i += 1
 
         def jobsRunning(self, job_ids: List[str]) -> bool:
--- cluster/SLURM/job_template.py
+++ cluster/SLURM/job_template.py
@@ -1,4 +1,4 @@
-# This string forms the first lines of every job file when using Sun Grid Engine scheduler. MEMORY is replaced by
+# This string forms the first lines of every job file when using Slurm Workload Manager scheduler. MEMORY is replaced by
 # RosettaDockMemory or ProtacModelMemory. Instead of HEADER the content of the file which SCHEDULER_PARAMS points to
 # is injected (line is removed if SCHEDULER_PARAMS is not set).
 job_template = '#!/bin/bash\n' \
